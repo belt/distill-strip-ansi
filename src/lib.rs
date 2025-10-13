eab89e3eeb2bb93c627330eb50f8e8a73e799601
Paul Belt <paul.belt+github@gmail.com>
2025-10-13 09:48:00 -0400

    feat: public API surface, lib.rs exports, no_std support

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![forbid(unsafe_code)]
+
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+mod parser;
+mod stream;
+mod strip;
+#[cfg(feature = "std")]
+mod writer;
+
+#[cfg(feature = "filter")]
+mod classifier;
+#[cfg(feature = "filter")]
+mod classify_tables;
+
+pub use parser::{Action, Parser, State};
+pub use stream::StripStream;
+pub use strip::{
+    contains_ansi, contains_ansi_c1, strip, strip_ansi_bytes, strip_ansi_escapes, strip_in_place,
+    strip_into, strip_str, try_strip_str,
+};
+#[cfg(feature = "std")]
+pub use writer::StripWriter;
+
+#[cfg(feature = "filter")]
+mod filter;
+
+#[cfg(feature = "filter")]
+mod stats;
+
+#[cfg(feature = "filter")]
+pub use classifier::{
+    ClassifyingParser, OscType, SeqAction, SeqDetail, SeqGroup, SeqKind, SgrContent, map_osc_number,
+};
+
+#[cfg(feature = "filter")]
+pub use filter::{
+    FilterConfig, FilterMode, FilterStream, filter_strip, filter_strip_into, filter_strip_str,
+    try_filter_strip_str,
+};
+
+#[cfg(feature = "filter")]
+pub use stats::{KindCount, Stats, ThreatHit};
+
+#[cfg(feature = "filter")]
+mod preset;
+
+#[cfg(feature = "filter")]
+pub use preset::TerminalPreset;
+
+#[cfg(feature = "terminal-detect")]
+mod detect;
+
+#[cfg(feature = "terminal-detect")]
+pub use detect::{
+    detect_preset, detect_preset_untrusted, detect_sgr_mask, detect_sgr_mask_untrusted,
+};
+
+#[cfg(feature = "toml-config")]
+mod toml_config;
+
+#[cfg(feature = "toml-config")]
+pub use toml_config::{ConfigError, FilterToml, GeneralConfig, StripAnsiConfig};
+
+#[cfg(feature = "toml-config")]
+mod threat_db;
+
+#[cfg(feature = "toml-config")]
+pub use threat_db::{ThreatDb, ThreatEntry};
+
+#[cfg(feature = "downgrade-color")]
+pub mod downgrade;
+
+#[cfg(feature = "transform")]
+pub mod sgr_rewrite;
+
+#[cfg(feature = "transform")]
+mod transform_stream;
+
+#[cfg(feature = "transform")]
+pub use transform_stream::{TransformConfig, TransformSlice, TransformSlices, TransformStream};
+
+#[cfg(feature = "augment-color")]
+pub mod palette;
+
+#[cfg(feature = "unicode-normalize")]
+pub mod unicode_map;
